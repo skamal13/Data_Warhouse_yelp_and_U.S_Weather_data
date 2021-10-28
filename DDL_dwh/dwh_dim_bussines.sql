e48ffd8ad1c1343a1d89979bd32e20686932a7db
syamsul kamal <samsort574@gmail.com>
2021-10-28 20:38:12 +0700

    Create dwh_dim_bussines.sql

---
+++ DDL_dwh/dwh_dim_bussines.sql
@@ -0,0 +1,12 @@
+CREATE TABLE dwh_dim_bussines (
+	business_id varchar NULL,
+	"name" varchar NULL,
+	address text NULL,
+	city varchar NULL,
+	state varchar NULL,
+	postal_code varchar NULL,
+	latitude float8 NULL,
+	longitude float8 NULL,
+	is_open int4 NULL
+);
+ALTER TABLE dwh_dim_bussines ADD CONSTRAINT dwh_dim_bussines_pk PRIMARY KEY (business_id);
